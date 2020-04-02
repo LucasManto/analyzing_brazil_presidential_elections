715466019715faa8228dba738a4b23a401136713
Lucas Mantovani <lucashmantovani@hotmail.com>
2020-04-02 17:29:21 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This work aims to use eletoral data as well as census data to evaluate the existence of space-time pattterns on brazilians presidential elections',
+    author='Lucas Henrique Mantovani Jacintho',
+    license='MIT',
+)
